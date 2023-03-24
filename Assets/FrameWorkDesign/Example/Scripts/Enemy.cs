7f51c0c401261fca644e386424dbdecaaa5c954e
Dailin <3452720699@qq.com>
2023-03-24 16:56:07 +0800

    快速开始： 点击开始->生成10个敌人->依次点击敌人至全部消失->显示游戏胜利界面 其他：制作自定义添加命名空间的功能，方便代码管理

---
+++ Assets/FrameWorkDesign/Example/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+namespace FrameWorkDesign.Example
+{
+    public class Enemy : MonoBehaviour
+    {
+        public GameObject GamePassPanel;
+        static int mKilledEnemyCount = 0;
+        public void OnMouseDown()
+        {
+            Destroy(gameObject);
+            mKilledEnemyCount++;
+            if (mKilledEnemyCount == 10)
+                GamePassPanel.SetActive(true);
+        }
+    }
+}
