83cd74032f0a371cebb78af288b9936f654363f5
Dailin <3452720699@qq.com>
2023-03-29 17:15:23 +0800

    模块化优化-引入 IOC 容器
     *简单理解为一个字典，以Type为Key, 以对象Instance为value。

---
+++ Assets/FrameWorkDesign/Example/IOC/IOCExample.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FrameWorkDesign.Example
+{
+    public class IOCExample : MonoBehaviour
+    {
+        IOCContainer container = new();
+        void Start()
+        {
+            container.Register(new BluetoothManager());
+            var blutoothManager = container.Get<BluetoothManager>();
+            blutoothManager.Connect();
+        }
+
+        class BluetoothManager
+        {
+            public void Connect()
+            {
+                Debug.Log("连接成功");
+            }
+        }
+    }
+}
---
+++ Assets/FrameWorkDesign/FrameWork/IOC/IOCContainer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+namespace FrameWorkDesign
+{
+    public class IOCContainer
+    {
+        private Dictionary<Type, object> mInstances = new();
+        public void Register<T>(T instance) where T : class
+        {
+            var key = typeof(T);
+            if (mInstances.ContainsKey(key))
+            {
+                mInstances[key] = instance;
+            }
+            else
+            {
+                mInstances.Add(key, instance);
+            }
+        }
+        public T Get<T>() where T : class
+        {
+            var key = typeof(T);
+            if (mInstances.TryGetValue(key, out object retInstance))
+            {
+                return retInstance as T;
+            }
+            return null;
+        }
+    }
+}
